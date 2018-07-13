8f7e605581be612339f03b5140455443a25947e9
Roy Prigat <rp2719@columbia.edu>
2018-07-13 12:21:25 +0300

    committing changes Fri Jul 13 12:21:25 IDT 2018

--- server.js
+++ server.js
@@ -11,8 +11,4 @@ app.get("/icons/list", (req, res) => {
     res.sendFile(__dirname + '/list.json');
 });
 
-app.post("/icon/add/:filename", (req,res) => {
-    
-});
-
-app.listen(app.get('port'), () => console.log('Server listening on port 3000...'));
+app.listen(app.get('port'), () => console.log('Server started...'));
